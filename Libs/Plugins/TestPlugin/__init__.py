991cd61561e75d92964176ad2cabf97b03ccabd7
CityDoomed <gorodage@gmail.com>
2016-06-23 23:26:08 +0300

    initial commit

--- Libs/Plugins/TestPlugin/__init__.py
+++ Libs/Plugins/TestPlugin/__init__.py
@@ -12,16 +12,18 @@ from Libs.uix.kdialog import KDialog
 def test_plugin(interval):
     action_view = app.start_screen.action_view
     item_button = \
-        ActionButton(id="plugins", on_press=events_screen,
-                     icon="{}/Libs/Plugins/TestPlugin/button.png".format(
+        ActionButton(id='plugins', on_press=events_screen,
+                     icon='{}/Libs/Plugins/TestPlugin/button.png'.format(
                          app.directory))
     action_view.add_widget(item_button, index=-1)
     action_view.add_widget(ActionSeparator(), index=-1)
 
 
 def events_screen(button):
-    message = KDialog(title="Plugin")
-    message.show(text="[color=#ffffff]Привет, плагин!")
+    KDialog(title='Plugin').show(
+        text='[color=#ffffff]Привет, [color=#2fa7d4ff]{}!'.format(
+            app.started_plugins['TestPlugin']['plugin-name']
+    ))
 
 
 Clock.schedule_once(test_plugin, 5)
